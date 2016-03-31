4cbdf9ee53a50844106e07c9ac7df6925998618e
paha710 <paha710@yandex.ru>
2016-03-31 13:46:02 +0300

    доделан анализатор , добавлены комменты

--- node_lesson/analysis.js
+++ node_lesson/analysis.js
@@ -3,10 +3,12 @@
  */
 'use strict';
 const colors = require('colors');
-
 const fs = require('fs');
+
+//значения побед и поражений по умолчанию
 let win = 0;
 let loss = 0;
+
 fs.readFile('log.txt', (err, data)=> {
     if (err) throw err;
     let res = data.toString();
@@ -18,21 +20,31 @@ fs.readFile('log.txt', (err, data)=> {
             loss++;
         }
     }
-
+// функция возвращает мак.серии, v = значение победы или поражения
     function max_count(v) {
-        let m = res[0], count = '', count_win = 0;
+
+        // значения по умолчанию m = начальный символ, count = счетчик,
+        // count_series = счетчик серии
+        let m = res[0], count = '', count_series = 0;
         for (let i = 0, j = res_length; i < j; i++) {
+
+            // если символ равен предыдущему и равен значению v,
+            // увеличиваем счетчик на 1 и если он больше счетчика серии
+            // увеличиваем счетчик серии на 1
+
             if ((res[i] == m) && res[i] == v) {
                 count++;
-                if (count > count_win) {
-                    count_win++;
+                if (count > count_series) {
+                    count_series++;
                 }
             } else {
+                // сбрасываем счетчик
                 count = 1;
             }
+            // присваеваем начальному символу текущее значение
             m = res[i];
         }
-        return count_win;
+        return count_series;
     }
 
     console.log(
